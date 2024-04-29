90613f70eeeab1073aa375a3ea9267cc9206477d
Naminar <buravko.va@phystech.edu>
2024-04-29 22:24:13 +0300

    Updated version of TLB without 'set' module.

--- set_tb.v
+++ set_tb.v
@@ -5,14 +5,15 @@ module set_tb;
     reg [63:0] pa;
     reg [11:0] pcid;
     wire [63:0] o_addr;
-    wire [7:0] hit;
-
-    cache TLB(clk, va, pa, pcid, o_addr, hit);
+    wire hit;
+    wire miss;
+    
+    cache TLB(clk, va, pa, pcid, o_addr, hit, miss);
 
     initial begin
         $dumpfile("set_tb.vcd");
         $dumpvars(0,set_tb);
-        $monitor("%t | clk = %d | out = %d", $time, clk, TLB._set_0.va[7]);
+        $monitor("%t | clk = %d | out = %d", $time, clk, TLB.tag_way7[3'd7]);
 
         clk = 0;
         pcid = 12'h0;
@@ -24,7 +25,7 @@ module set_tb;
         va = 64'h0;
         pa = 0;
         #10 
-        va = 64'hfffffffffffffff2;
+        va = 64'hfffffffffffffff1;
         pa = 0;
 
         #100 $finish;
