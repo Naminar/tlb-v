82cc137419a18bc6659ac34414f4341a0886376b
Naminar <buravko.va@phystech.edu>
2024-05-04 00:03:11 +0300

    PMU and MMU correctness fixed.

--- mmu.v
+++ mmu.v
@@ -2,8 +2,8 @@ module mmu
 (
     input translate, // if miss in tlb or significant request
     input [63:0] va,
-    input [11:0] pcid
-    ouput [63:0] ta
+    input [11:0] pcid,
+    output [63:0] ta
 );
     
 
