b75cda1b6f0ef9d36d26bb52f4918f69f2963b2d
Naminar <buravko.va@phystech.edu>
2024-05-09 12:35:53 +0300

    Project expansion:STLB
    Added new statistic for second level cache.

--- pmu.v
+++ pmu.v
@@ -1,23 +1,37 @@
 
-module pmu
+module PMU
 (   input clk,
     input tlb_hit,
     input tlb_miss,
     input tlb_prefetch,
+    input stlb_hit,
+    input stlb_miss,
+    input stlb_prefetch,
     output [63:0] out1,
     output [63:0] out2,
     output [63:0] out3
 );
 
-reg [2:0] prev_state_reg = 0;
+reg [5:0] prev_state_reg = 0;
 
 reg unsigned [63:0] dTLB_hit      = 0;
 reg unsigned [63:0] dTLB_miss     = 0;
 reg unsigned [63:0] dTLB_prefetch = 0;
+reg unsigned [63:0] STLB_hit      = 0;
+reg unsigned [63:0] STLB_miss     = 0;
+reg unsigned [63:0] STLB_prefetch = 0;
 
 assign out1 = dTLB_hit;
 assign out2 = dTLB_miss;
 assign out3 = dTLB_prefetch;
+
+// dir_bit - direct bit;
+// f_signal - flag signal;
+// s_signal - stat signal;
+`define GEN_STAT(dir_bit, f_signal, s_signal)\
+if (prev_state_reg[dir_bit] == 1'b0 && f_signal == 1'b1)\
+        s_signal <= s_signal + 1'b1;\
+    prev_state_reg[dir_bit] <= f_signal;\
 
 always @(posedge clk) begin
 
@@ -32,6 +46,10 @@ always @(posedge clk) begin
     if (prev_state_reg[2] == 1'b0 && tlb_prefetch == 1'b1)
         dTLB_prefetch <= dTLB_prefetch + 1'b1;   
     prev_state_reg[2] <= tlb_prefetch;
+
+    `GEN_STAT(3, stlb_hit, STLB_hit)
+    `GEN_STAT(4, stlb_miss, STLB_miss)
+    `GEN_STAT(5, stlb_prefetch, STLB_prefetch)
 end
     
 endmodule
